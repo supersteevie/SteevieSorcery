b0371538017143ad4e2392d5795449bcdc57b3c2
Stephen Larsen <maxpendragon@gmail.com>
2016-04-19 15:58:04 -0600

    Animation scripting stuff.

---
+++ United Sorcery/Assets/AnimationClips/BenderAnimate.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BenderAnimate : MonoBehaviour {
+
+    private Animator benderAnim;
+
+	// Use this for initialization
+	void Start () {
+        benderAnim = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void AddSlash()
+    {
+
+    }
+}
