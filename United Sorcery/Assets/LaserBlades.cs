68526beed676f6c3336b1636471d65b299469094
Stephen Larsen <maxpendragon@gmail.com>
2016-04-18 19:20:02 -0600

    Working on stuff.

---
+++ United Sorcery/Assets/LaserBlades.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserBlades : MonoBehaviour {
+
+    public ParticleSystem bladeL;
+    public ParticleSystem bladeR;
+
+    public float sparkAmnt;
+
+    private Animator bladeAnim;
+
+	// Use this for initialization
+	void Start () {
+        bladeAnim = gameObject.GetComponent<Animator>();
+ 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnSwing() {
+      
+
+    }
+}
